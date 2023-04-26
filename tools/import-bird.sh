1e9b6872c514e9530e96e38e5eddbc90a927359c
Connor Demille <subtlepseudonym@gmail.com>
2023-04-26 14:52:46 -0400

    Update bird import tool to write date-num names

--- tools/import-bird.sh
+++ tools/import-bird.sh
@@ -14,16 +14,18 @@ settings="$(
 width="$(exiftool -s3 -ImageWidth "$1")"
 height="$(exiftool -s3 -Imageheight "$1")"
 
-filename="$(basename "$1" | gawk 'gsub(/_/, "-", $0) match($0, /(.*)-([0-9]{8}-[0-9]+\..*)/, a) { print a[1] "/" a[2] }')"
+target_path="$(basename "$1" | gawk 'gsub(/_/, "-", $0) match($0, /(.*)-([0-9]{8}-[0-9]+\..*)/, a) { print a[1] "/" a[2] }')"
+bird_dir="$(dirname ${target_path})"
+image_filename="$(basename ${target_path})"
+thumb_filename="$(gawk 'match($0, /(.*)\.(.*)$/, a) { print a[1] "-thumb." a[2] }' <<< ${image_filename})"
+page_filename="$(gawk 'match($0, /(.*)\.(.*)$/, a) { print a[1] ".md" }' <<< ${image_filename})"
 
-target_dir="content/projects/bird-photography/$(dirname ${filename})"
+target_dir="content/projects/bird-photography/${bird_dir}"
 mkdir -p "${target_dir}"
-cp "$1" "${target_dir}/$(basename ${filename})"
+cp "$1" "${target_dir}/${image_filename}"
+ffmpeg -i "${target_dir}/${image_filename}" -vf scale=840:-1 "${target_dir}/${thumb_filename}"
 
-thumb_filename="$(gawk 'match($0, /(.*)\.(.*)$/, a) { print a[1] "-thumb." a[2] }' <<< $(basename ${filename}))"
-ffmpeg -i "${target_dir}/$(basename ${filename})" -vf scale=840:-1 "${target_dir}/${thumb_filename}"
-
-printf "##### INPUT #####\nweight: "
+printf "\n##### INPUT #####\nweight: "
 read -r weight
 
 printf "description: "
@@ -31,8 +33,8 @@ read -r description
 
 echo "---
 weight: ${weight}
-image: \"${filename}\"
+image: \"${bird_dir}/${image_filename}\"
 width: ${width}
 height: ${height}
 description: \"${description}<br/>${settings}\"
----" >> "${target_dir}/index.md"
+---" >> "${target_dir}/${page_filename}"
